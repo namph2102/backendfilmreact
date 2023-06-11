5106b188bbf4de85a25f019811d7e636c2e40055
Nam <namph2102@gmail.com>
2023-06-11 07:38:47 +0700

    fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { engine } = require("express-handlebars");
 
 // sử dụng view engine
